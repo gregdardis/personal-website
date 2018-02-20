deb26fd0db5426a85274852ea60cb6f07363edff
Greg Dardis <dardis.greg@gmail.com>
2018-02-20 15:10:51 -0700

    created technologies component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './home.css';
 import About from '../about';
 import NameBanner from '../nameBanner';
+import Technologies from '../technologies';
 
 const Home = () => (
   <div className='home'>
     <NameBanner />
     <About />
+    <Technologies />
   </div>
 );
 
---
+++ src/components/technologies/Technologies.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './technologies.css';
+
+const Technologies = () => (
+  <div className='technologies'>
+    <h2>Technologies</h2>
+    <h3>- Java/Android</h3>
+    <h3>- JavaScript (ES6)</h3>
+    <h3>- HTML5 and CSS3</h3>
+    <h3>- React/Redux</h3>
+    <h3>- Git</h3>
+  </div>
+);
+
+export default Technologies;
