50b8087e0e54d9ea585e43b5b1742d60a782bcee
Greg Dardis <dardis.greg@gmail.com>
2018-02-20 15:30:35 -0700

    created RightColumn component

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,7 +6,7 @@ const About = () => (
   <div className='about'>
     <h2>About Me</h2>
     <p>
-      Currently a student at the University of Alberta, graduating May 2018
+      I am currently a student at the University of Alberta, graduating May 2018
       with a BSc in Mathematics, minor in chemistry.
       <b> I am looking for an entry-level software engineering position in
       Vancouver, BC starting May 2018 or after.</b>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import './home.css';
 import NameBanner from '../nameBanner';
-import Technologies from '../technologies';
 import LeftColumn from '../leftColumn';
+import RightColumn from '../rightColumn';
 
 const Home = () => (
   <div className='home'>
     <NameBanner />
     <LeftColumn />
-    <Technologies />
+    <RightColumn />
   </div>
 );
 
