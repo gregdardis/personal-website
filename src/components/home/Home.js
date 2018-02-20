90b706b4b3f5ad0f06d09e74b5a84ebcb4be7bac
Greg Dardis <dardis.greg@gmail.com>
2018-02-20 13:22:12 -0700

    added a photo, made an About Me section

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 import './home.css';
 import About from '../about';
+import hiking from '../../images/hiking.jpg';
 
 const Home = () => (
   <div className='home'>
+    <img src={ hiking } alt='Hiking in the Rocky Mountains'
+      title='Hiking in the Rocky Mountains' />
     <About />
   </div>
 );
