8464d14e4b6cb0ce3ac5e0fc77f2e9aeb41d2401
Greg Dardis <dardis.greg@gmail.com>
2018-02-20 15:41:20 -0700

    added some technologies

--- src/components/technologies/Technologies.js
+++ src/components/technologies/Technologies.js
@@ -5,11 +5,20 @@ import './technologies.css';
 const Technologies = () => (
   <div className='technologies'>
     <h2>Technologies</h2>
-    <h3>- Java/Android</h3>
-    <h3>- JavaScript (ES6)</h3>
-    <h3>- HTML5 and CSS3</h3>
-    <h3>- React/Redux</h3>
-    <h3>- Git</h3>
+    <h3>Java</h3>
+    <h4>- Android</h4>
+    <h4>- JUnit</h4>
+    <h4>- Eclipse</h4>
+    <h3>JavaScript (ES6)</h3>
+    <h4>- Node/Express</h4>
+    <h4>- React/Redux</h4>
+    <h4>- Webpack/Babel</h4>
+    <h4>- HTML5 and CSS3</h4>
+    <h3>General</h3>
+    <h4>- Git</h4>
+    <h4>- VS Code</h4>
+    <h4>- Atom</h4>
+    <h4>- SQLite</h4>
   </div>
 );
 
