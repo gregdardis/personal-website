e6724b52383bb894bf084f4d424bcbd08decce4f
Greg Dardis <dardis.greg@gmail.com>
2018-03-02 10:56:31 -0700

    removed actions, reducers folders. made NavBar react class instead of stateless functional component

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-
-import { reducer as selectedPageUrl } from './selectedPageUrl';
-
-const reducer = combineReducers({
-  selectedPageUrl
-});
-export default reducer;
