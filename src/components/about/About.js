62448729e36a7bd90fdb7c4c1ed4d955e3a694c1
Greg Dardis <dardis.greg@gmail.com>
2018-06-20 08:35:58 -0700

    updated aboutMe section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,7 +6,7 @@ const About = () => (
   <div className='about'>
     <h2>About Me</h2>
     <p>
-      I am currently a student at the University of Alberta, graduating May 2018
+      I am a recent graduate from the University of Alberta, graduated May 2018
       with a BSc in Mathematics, minor in Chemistry.
       <b> I am looking for an entry-level software engineering position in
       Vancouver, BC.</b>
@@ -16,9 +16,17 @@ const About = () => (
       beginning with Stanford's CS106A course (all materials are online), taught
       in Java. From there I co-created an Android fitness app called Lifter Log,
       then started taking a Coursera course to learn algorithms and data
-      structures. More recently I co-created a web app where the user can
-      search for stocks in Yahoo Finance's database, which was great experience
-      using Node, Express, React/Redux, and other current web technologies.
+      structures.
+    </p>
+    <p>
+      To begin my web development journey I co-created a web app
+      where the user can search for stocks in Yahoo Finance's database,
+      which was great experience using Node, Express, React/Redux,
+      and other current web technologies. Since then, I created a
+      calculator for investors to analyze rental properties, and
+      this was mainly React+Redux with Node/Express for the server.
+      The calculator supports user authentication to save rental property
+      analyses, implemented with JWT and passport.
     </p>
   </div>
 );
