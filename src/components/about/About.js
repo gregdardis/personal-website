872daf383cfe084e428f7796f86b1f8d02d3dc02
Greg Dardis <dardis.greg@gmail.com>
2018-08-02 09:06:37 -0700

    updated about me section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -22,11 +22,17 @@ const About = () => (
       To begin my web development journey I co-created a web app
       where the user can search for stocks in Yahoo Finance's database,
       which was great experience using Node, Express, React/Redux,
-      and other current web technologies. Since then, I created a
+      and other current web technologies. Afterwards, I created a
       calculator for investors to analyze rental properties, and
       this was mainly React+Redux with Node/Express for the server.
       The calculator supports user authentication to save rental property
       analyses, implemented with JWT and passport.
+    </p>
+    <p>
+      Lately, my passion has brought me to C# and ASP.NET Core, where I
+      created a Fitness API to access a database of information about
+      weightlifting exercises. I am excited to improve my skills with ASP.NET
+      and server-side programming in the future.
     </p>
   </div>
 );
